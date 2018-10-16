356064a3753d591f2260d1b7f854540791d9e891
brunoagatao <brunoagatao@hotmail.com>
2018-10-16 11:11:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ba-calendar
+
+Calendar web application built with Vue.js
+
+[[App Demo](https://ba-calendar.herokuapp.com)]
+
+Based on Anthony Gore's [Udemy course](https://www.udemy.com/vuejs-2-essentials/).
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+
+const app = express();
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const template = fs.readFileSync(path.resolve('./src/index.html'), 'utf-8');
+app.get('/', (req, res) => {
+  res.send(template);
+});
+
+const server = http.createServer(app);
+
+if (process.env.NODE_ENV === 'development') {
+  require('./webpack-dev-middleware').init(app);
+  const reload = require('reload');
+  const reloadServer = reload(server, app);
+}
+
+const port = process.env.PORT || 3000;
+server.listen(port, function() {
+  console.log(`Listening on port ${port}!`);
+  if (process.env.NODE_ENV === 'development')
+    require('open')(`http://localhost:${port}`);
+});
