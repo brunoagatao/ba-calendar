f0a57d77691fcb76a5f4ef777608d517e3e71483
brunoagatao <brunoagatao@hotmail.com>
2018-10-20 16:04:43 +0100

    Added the month selector

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
 import moment from 'moment-timezone';
+import store from './store';
 import './style.scss';
 
 import App from './components/App.vue';
-
-Vue.use(Vuex);
 
 moment.tz.setDefault('UTC');
 Object.defineProperty(Vue.prototype, '$moment', {
@@ -22,10 +20,5 @@ new Vue({
   components: {
     App
   },
-  store: {
-    state: {
-      currentYear: 2018,
-      currentMonth: 10
-    }
-  }
+  store
 });
